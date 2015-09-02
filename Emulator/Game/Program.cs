e7ee9ca78eaffdfe1b25850b89d362cba1225d54
toxiicdev <toxiicpp@gmail.com>
2015-09-02 15:16:23 +0200

    Implemented Shared Project Data

---
+++ Emulator/Game/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
